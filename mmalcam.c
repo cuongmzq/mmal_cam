beec083edc07d2cd12553e38f322d21742aea86b
cuongmzq <63182706+cuongmzq@users.noreply.github.com>
2023-03-20 21:09:19 +0700

    Update mmalcam.c

--- mmalcam.c
+++ mmalcam.c
@@ -82,7 +82,7 @@ int main(int argc, const char **argv)
    camcorder_behaviour.bit_rate = DEFAULT_BIT_RATE;
    camcorder_behaviour.focus_test = MMAL_PARAM_FOCUS_MAX;
    camcorder_behaviour.camera_num = DEFAULT_CAM_NUM;
-   camcorder_behaviour.frame_cb = [](MMAL_BUFFER_HEADER_T *frame) {
+   camcorder_behaviour.frame_cb = []()(MMAL_BUFFER_HEADER_T *frame) {
       printf("Frame Size: %d\n", frame->length);
    };
 
